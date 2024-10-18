5bae6c841be9a7ca9c2700931d94efd142c2bc0f
zarila00 <shyreenkhoza8@gmail.com>
2024-10-18 05:32:49 -0700

    drinks

--- src/drinks/controllers/drinks/drinks.controller.ts
+++ src/drinks/controllers/drinks/drinks.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { DrinksService } from 'src/drinks/services/drinks/drinks.service';
+import { drinksDTO } from 'src/DTOs/drinks.DTO';
 import { drinksEntity } from 'src/Entities/drinks.Entity';
 
 
@@ -22,4 +23,14 @@ export class DrinksController {
         return this.drinksService.createDrinks(drinksEntity)
 
     }
+
+    @Get()
+    @ApiOperation({summary:'creating the drinks'})
+    @ApiResponse({status:201, description:'drinks creates'})
+
+    getDrink():Promise<drinksDTO[]>{
+        return this.drinksService.findDrinks()
+    }
+
+
 }
