e4e527bbb9d84247698f45917ad81c2a29ba93c5
zarila00 <shyreenkhoza8@gmail.com>
2024-10-18 19:59:28 -0700

    done

--- src/pharmacy/controllers/pharmacy/pharmacy.controller.ts
+++ src/pharmacy/controllers/pharmacy/pharmacy.controller.ts
@@ -24,11 +24,13 @@ export class PharmacyController {
 
     }
     @Get()
-    @ApiOperation({summary:'pharmacy post made'})
-    @ApiResponse({status:201, description:'created'})
-    getAll() :Promise<pharmarcyDTO[]>{
+    @ApiOperation({summary:'creating the drinks'})
+    @ApiResponse({status:201, description:'drinks creates'})
+
+    getDrink():Promise<pharmarcyDTO[]>{
         return this.pharmarcyService.findPharmarcy()
     }
+
     @Put(':id')
     @ApiParam({name: 'id', description:'the ID of the item', type:Number})
     @ApiOperation({summary:'update the market item'})
